fea6f410ebd2c90cf8521044de691fb4811ddfc7
abdul-Hannan232 <abdulhani232@gmail.com>
2024-04-30 18:29:02 +0500

    Changes in home page"

--- src/app/page.js
+++ src/app/page.js
@@ -20,7 +20,7 @@ const Home = () => {
         <button className='text-[#0B5CFF] bg-[#0B5CFF] bg-opacity-30 md:rounded-xl rounded-md cursor-pointer md:p-3 p-1 md:w-24 w-20 font-medium md:text-md text-sm text-center'>View All</button>
       </div>
         <PopularCars Data={HomeData} grid={"xl:grid-cols-4"} color={"#0B5CFF"}   textColor={'text-[#FFFFFF]'} />
-        <button className=' buttonBackground md:w-60 w-40 md:p-5 p-3 rounded-2xl  text-center text-lg text-white  flex justify-center mx-auto mt-10 mb-32'>Show more cars</button>
+        <button className=' buttonBackground md:w-60 w-44 md:p-5 p-3 rounded-2xl  text-center md:text-lg text-sm text-white  flex justify-center mx-auto mt-10 mb-32'>Show more cars</button>
       </div>
     </>
   )
